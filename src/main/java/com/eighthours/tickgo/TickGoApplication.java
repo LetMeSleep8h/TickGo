f3eb3be6880eec9ff4ec4f550219137f4035e20d
pengzeyu <dhsm20230725@163.com>
2026-05-30 10:34:19 +0800

    init

---
+++ src/main/java/com/eighthours/tickgo/TickGoApplication.java
@@ -0,0 +1,13 @@
+package com.eighthours.tickgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eighthours/tickgo/TickGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eighthours.tickgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
